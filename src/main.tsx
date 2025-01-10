28497df8c3ff55f18e1f4b64a75b20d983b355d2
SamuelBarbosaDev <samueloficial@protonmail.com>
2025-01-10 07:57:53 -0300

    routes: Substituindo `HashRouter` por `BrowserRouter`

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import { Home } from './pages/home';
 import { Menu } from './components/menu';
 import { createRoot } from 'react-dom/client';
 import { Footer } from './components/footer';
-import { HashRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Menu />
             <Routes>
                 <Route path="/" Component={Home} />
             </Routes>
             <Footer />
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>,
 );
